24236492ac6269042e9f0b63be8437318c3020ee
Nurel <comp80269@gmail.com>
2026-08-25 10:16:49 +0600

    Remove cart module dependencies

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
